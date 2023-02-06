cf460306cafa7f4876716ac35c49139b91bfe788
arapt <arapt@DESKTOP-70PT2F0>
2023-02-06 15:36:03 -0500

    Add project files.

---
+++ bookStore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace bookStore
+{
+    class myStore
+    {
+        static void Main(string[] args)
+        {
+            book book1 = new book();
+        }
+
+
+    }
+
+    class book
+    {
+        private int _Id;
+        private string _Title;
+        private string _Author;
+        public void SetId(int newId)
+        {
+            _Id = newId;
+        }
+        public void SetTitle(string newTitle)
+        {
+
+        }
+        public void SetTitle(string newTitle)
+        {
+
+        }
+    }
+}
